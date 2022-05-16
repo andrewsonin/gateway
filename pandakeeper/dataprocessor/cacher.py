8a2319dd896cd471b03567366bf103cdb078a31d
Andrew Sonin <sonin.cel@yandex.ru>
2022-05-16 15:37:11 +0300

    Add DataCacher

---
+++ pandakeeper/dataprocessor/cacher.py
@@ -0,0 +1,59 @@
+from pandas import DataFrame
+from typing_extensions import final
+
+from pandakeeper.dataprocessor import DataProcessor
+
+__all__ = (
+    'AbstractDataCacher',
+    'RuntimeCacher',
+)
+
+
+class AbstractDataCacher(DataProcessor):
+    """DataProcessor that caches input data."""
+    __slots__ = ()
+
+    @final
+    @property
+    def use_cached(self) -> bool:
+        return True
+
+
+class RuntimeCacher(AbstractDataCacher):
+    """DataCacher for caching single Node output."""
+    __slots__ = ('__dataframe',)
+
+    @final
+    def _dump_to_cache(self, data: DataFrame) -> None:
+        self.__dataframe = data
+
+    @final
+    def _clear_cache_storage(self) -> None:
+        self.__dataframe = None
+
+    @final
+    def _load_cached(self) -> DataFrame:
+        df = self.__dataframe
+        if df is not None:
+            return df
+        raise ValueError("Cannot load non-cached data")
+
+    @final
+    def _load_non_cached(self) -> DataFrame:
+        pnc = self.positional_node_connections
+        nnc = self.named_node_connections
+        total_dfs = len(nnc) + len(pnc)
+        if total_dfs != 1:
+            raise ValueError(
+                "Cannot be connected to more or less than one Node. "
+                f"Actual number of connections: {total_dfs}"
+            )
+        try:
+            node_connection = pnc[0]
+        except IndexError:
+            node_connection = next(iter(nnc.values()))
+        return node_connection.extract_data()
+
+    @final
+    def transform_data(self, data: DataFrame) -> DataFrame:
+        return data
